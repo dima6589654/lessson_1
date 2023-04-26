31151eac4f23e0efa4334429eeac37f057bf9c61
Дмитрий Кан <@itstep.loc>
2023-04-26 17:24:55 +0600

    lesson_04

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -34,6 +34,6 @@ def by_rubric(request, rubric_id):
     context = {
         'bbs': bbs,
         'rubrics': rubrics,
-        'current_rubric': current_rubric
+        'current_rubric': current_rubric,
     }
     return render(request, 'bboard/by_rubric.html', context)
