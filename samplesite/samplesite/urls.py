10afdbd9d3450fa64c482f45d5ef1cf3cd978c1e
Дмитрий Кан <@itstep.loc>
2023-05-03 17:22:30 +0600

    lesson5

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bboard/', include('bboard.urls')),
+    path('', include('bboard.urls')),
 ]
 
