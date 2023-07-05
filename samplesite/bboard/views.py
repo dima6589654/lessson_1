b0c592d16b9f2daa01df264eed39ea5f601b262d
Дмитрий Кан <@itstep.loc>
2023-07-05 15:21:51 +0600

    lesson_19

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,17 +1,14 @@
-from django.db.models import Min, Max, Count, Q, Sum, IntegerField, Avg
-from django.http import HttpResponseRedirect, HttpResponse, \
-    HttpResponseNotFound, Http404, StreamingHttpResponse, FileResponse, \
-    JsonResponse
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from django.template.loader import get_template, render_to_string
-from django.urls import reverse_lazy, reverse
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView, FormView, UpdateView, DeleteView
-
 from bboard.forms import BbForm
 from bboard.models import Bb, Rubric
+from django.db.models import Min, Max, Count
+from django.http import HttpResponseRedirect, HttpResponse, \
+    HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy, reverse
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, FormView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
 
 def count_bb():
@@ -69,70 +66,18 @@ def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
     context = {'bbs': bbs, 'rubrics': rubrics}
-    # template = get_template('bboard/index.html')
-    # return HttpResponse(template.render(context, request))
     return HttpResponse(render_to_string('bboard/index.html', context, request))
-    # data = {'title': 'Мотоцикл', 'content': 'Старый', 'price': 10000.0}
-    # return JsonResponse(data)
-    # return redirect('by_rubric', rubric_id=bbf.cleaned_data['rubric'].pk)
-
-
-
-# def index(request):
-#     resp_content = ('Здесь будет', ' главная', ' страница', ' сайта')
-#     resp = StreamingHttpResponse(resp_content,
-#                                  content_type='text/plain; charset=utf-8')
-#     return resp
-
-
 def index_old(request):
     bbs = Bb.objects.order_by('-published')
     rubrics = Rubric.objects.all()
-
-    # min_price = Bb.objects.aggregate(Min('price'))
-    # max_price = Bb.objects.aggregate(mp=Max('price'))
     result = Bb.objects.aggregate(min_price=Min('price'),
                                   max_price=Max('price'),
                                   diff_price=Max('price') - Min('price'))
 
-    # for r in Rubric.objects.annotate(Count('bb')):
-    #     print(r.name, ': ', r.bb__count, sep='')
-    #
-    # for r in Rubric.objects.annotate(num_bbs=Count('bb')):
-    #     print(r.name, ': ', r.num_bbs, sep='')
-
-    # for r in Rubric.objects.annotate(
-    #         cnt=Count('bb', filter=Q(bb__price__gt=1000))):
-    #                                  # min=Min('bb__price')).filter(cnt__gt=0):
-    #     print(r.name, ': ', r.cnt, sep='')
-    #     # print(r.name, ': ', r.min, sep='')
-
-    # print(
-    #     Bb.objects.aggregate(
-    #         sum=Sum(
-    #             'price',
-    #             output_field=IntegerField(),
-    #             filter=Q(rubric__name='Сельхозтехника')
-    #         )
-    #     )
-    # )
-
-    # print(
-    #     Bb.objects.aggregate(
-    #         avg=Avg(
-    #             'price',
-    #             output_field=IntegerField(),
-    #             filter=Q(rubric__name='Сельхозтехника'),
-    #             distinct=False  # если True, то только уникальные
-    #         )
-    #     )
-    # )
-
     context = {
         'bbs': bbs,
         'rubrics': rubrics,
-        # 'min_price': min_price.get('price__min'),
-        # 'max_price': max_price.get('mp'),
+
         'min_price': result.get('min_price'),
         'max_price': result.get('max_price'),
         'diff_price': result.get('diff_price'),
@@ -153,30 +98,15 @@ def by_rubric(request, rubric_id, **kwargs):
     bbs = Bb.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
 
-    # print(kwargs.get('name'), kwargs.get('beaver'))
-
     context = {
         'bbs': bbs,
         'rubrics': rubrics,
         'current_rubric': current_rubric,
         'count_bb': count_bb(),
-        # 'name': kwargs.get('name'),
         'kwargs': kwargs,
     }
 
     return render(request, 'bboard/by_rubric.html', context)
-
-
-# class BbByRubricView(TemplateView):
-#     template_name = 'bboard/by_rubric.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['current_rubric'] = Rubric.objects.get(pk=context['rubric_id'])
-#         context['bbs'] = Bb.objects.filter(rubric=context['rubric_id'])
-#         context['rubrics'] = Rubric.objects.all()
-#         return context
-
 
 def add(request):
     bbf = BbForm()
@@ -212,16 +142,6 @@ def add_and_save(request):
         bbf = BbForm()
         context = {'form': bbf}
         return render(request, 'bboard/create.html', context)
-
-
-# def detail(request, bb_id):
-#     try:
-#         bb = Bb.objects.get(pk=bb_id)
-#     except Bb.DoesNotExist:
-#         # return HttpResponseNotFound('Такого объявлениея нет')
-#         return Http404('Такого объявлениея нет')
-#     return HttpResponse(...)
-
 
 def detail(request, rec_id):
     bb = get_object_or_404(Bb, pk=rec_id)
@@ -273,5 +193,5 @@ class BbAddView(FormView):
         return self.object
 
     def get_success_url(self):
-        return reverse('bboard:by_rubric',
+        return reverse('by_rubric',
                        kwargs={'rubric_id': self.object.cleaned_data['rubric'].pk})
