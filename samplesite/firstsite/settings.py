28a53779e3596814fdf343a9aabc61052e236dc1
Дмитрий Кан <@itstep.loc>
2023-07-17 16:58:07 +0600

    lesson_23

--- samplesite/firstsite/settings.py
+++ samplesite/firstsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
             ],
         },
     },
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- samplesite/salad/urls.py
+++ samplesite/salad/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from salad.views import IndexSlView
 
 urlpatterns = [
-    path('', IndexSlView.as_view(), name='index'),
+    path('', IndexSlView.as_view(), name='index1'),
 
 ]
