e3b7eb31f471693fdc10283db372f8026dac7b1c
Дмитрий Кан <@itstep.loc>
2023-08-16 14:09:41 +0600

    lesson_25

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
-from bboard.models import Bb
+from bboard.models import Bb, Rubric
+from django.forms import modelform_factory, DecimalField
+from django.forms.widgets import Select
 
+# class BbForm(ModelForm):
+# class Meta:
+#     model = Bb
+#     fields = ('title', 'content', 'price', 'rubric')
 
-class BbForm(ModelForm):
-    class Meta:
-        model = Bb
-        fields = ('title', 'content', 'price', 'rubric')
+BbForm = modelform_factory(Bb,
+fields = ('title', 'content', 'price', 'rubric'),
+labels = {'title': 'Название товара'},
+help_texts = {'rubric': 'He забудьте выбрать рубрику!'},
+field_classes = {'price': DecimalField},
+widgets = {'rubric': Select(attrs={'size': 8})})
