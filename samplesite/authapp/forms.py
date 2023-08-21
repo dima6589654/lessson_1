a2d7193a3e32c7af6eeaee3cf458d3ac7c1688c4
Дмитрий Кан <@itstep.loc>
2023-08-21 14:30:34 +0600

    lesson_26

--- samplesite/authapp/forms.py
+++ samplesite/authapp/forms.py
@@ -1,18 +1,40 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
+
+
 # from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Логин',
+                               validators=[validators.RegexValidator(regex='^.{2,}$')],
+                               error_messages={'invalid': "Логин слишком короткий"})
+
     class Meta:
         model = User
         fields = ('username', 'password')
 
+    def clean_username(self):
+        val = self.cleaned_data['username']
+        if val == 'admin':
+            raise ValidationError("Такое имя пользователя не допускается")
+        return val
+
+    def clean_password(self):
+        val = self.cleaned_data['password']
+        if val == 'admin':
+            raise ValidationError("Такой пороль не допускается")
+        return val
+
 
 # class RegisterUserForm(forms.ModelForm):
 class RegisterUserForm(UserCreationForm):
-    # password1 = forms.CharField(label='Пароль')
-    # password2 = forms.CharField(label='Пароль (повторно)')
+    first_name = forms.CharField(label='Имя',
+                                 validators=[validators.RegexValidator(regex='^.{3,}$')],
+                                 error_messages={'invalid': "Имя пользователя слишком короткое"})
 
     class Meta:
         model = User
--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -1,16 +1,66 @@
+from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
+
 from bboard.models import Bb, Rubric
-from django.forms import modelform_factory, DecimalField
-from django.forms.widgets import Select
+
 
 # class BbForm(ModelForm):
-# class Meta:
-#     model = Bb
-#     fields = ('title', 'content', 'price', 'rubric')
+#     class Meta:
+#         model = Bb
+#         fields = ('title', 'content', 'price', 'rubric')
 
-BbForm = modelform_factory(Bb,
-fields = ('title', 'content', 'price', 'rubric'),
-labels = {'title': 'Название товара'},
-help_texts = {'rubric': 'He забудьте выбрать рубрику!'},
-field_classes = {'price': DecimalField},
-widgets = {'rubric': Select(attrs={'size': 8})})
+
+# BbForm = modelform_factory(Bb,
+#                            fields=('title', 'content', 'price', 'rubric'),
+#                            labels={'title': 'Название товара'},
+#                            help_texts={'rubric': 'Не забудьте выбрать рубрику!'},
+#                            field_classes={'price': DecimalField},
+#                            widgets={'rubric': Select(attrs={'size': 8})}
+#                            )
+
+
+# class BbForm(ModelForm):
+#     class Meta:
+#         model = Bb
+#         fields = ('title', 'content', 'price', 'rubric')
+#         labels = {'title': 'Название товара'},
+#         help_texts = {'rubric': 'Не забудьте выбрать рубрику!'},
+#         field_classes = {'price': DecimalField},
+#         widgets = {'rubric': Select(attrs={'size': 8})}
+
+
+class BbForm(ModelForm):
+    title = forms.CharField(label='Название товара',
+                            validators=[validators.RegexValidator(regex='^.{4,}$')],
+                            error_messages={'invalid': "Слишком короткое название товара"})
+
+    content = forms.CharField(label='Описание',
+                              widget=forms.widgets.Textarea())
+    price = forms.DecimalField(label='Цена', decimal_places=2)
+    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(),
+                                    label='Рубрика',
+                                    help_text='Не забудьте выбрать рубрику!',
+                                    widget=forms.widgets.Select(attrs={'size': 1,
+                                                                       'class': 'danger'}))
+
+    def clean_title(self):
+        val = self.cleaned_data['title']
+        if val == 'Снег':
+            raise ValidationError("не допускается")
+        return val
+
+    def clean(self):
+        super().clean()
+        errors = {}
+        if not self.cleaned_data['content']:
+            errors['content'] = ValidationError("укажите описание товара")
+            if self.cleaned_data['price'] <= 0:
+                errors['price'] = ValidationError("Укажите положительное число")
+                if errors:
+                    raise ValidationError(errors)
+
+    class Meta:
+        model = Bb
+        fields = ('title', 'content', 'price', 'rubric')
