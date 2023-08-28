d8330295870e81843013336b889c2a887dff21ed
Дмитрий Кан <@itstep.loc>
2023-08-28 17:43:33 +0600

    lesson_30

--- samplesite/salad/models.py
+++ samplesite/salad/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -18,3 +21,25 @@ class Sl(models.Model):
         blank=True,
         verbose_name="Цена",
     )
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+
+
+class NewSl(Sl):
+    title = models.CharField(max_length=80,
+                             verbose_name='Скидочный тавар')
+    content = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание",
+    )
+    price = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name="Цена со скидкой", )
+
+    class Meta:
+        pass
